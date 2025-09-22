57811f57ea6a789b315a03a5ff0f425c2846276c
Ian <Gibbu_@hotmail.com>
2025-09-22 11:07:10 +1000

    Task 5 - init

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,39 @@
+interface DirectorInteface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInteface {
+	workFromHome(): string {
+		return 'Working from home';
+	}
+	getCoffeeBreak(): string {
+		return 'Getting a coffee break';
+	}
+	workDirectorTasks(): string {
+		return 'Getting to director tasks';
+	}
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome(): string {
+		return 'Cannot work from home';
+	}
+	getCoffeeBreak(): string {
+		return 'Cannot have a break';
+	}
+	workTeacherTasks(): string {
+		return 'Getting to work';
+	}
+}
+
+function createEmployee(salary: Teacher | Director) {
+	return typeof salary === 'number' && salary < 500 ? new Teacher() : new Director();
+}
